68a71c4a4ec3f9be96685d549aa8cfb08d0a2766
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-11-29 15:12:33 +0100

    commit

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'http://localhost:4000' 
 
+// export const baseUrl = 'http://172.16.30.177:4000/' 
 export const localStorageJwtKey = 'currentUserJwt'
