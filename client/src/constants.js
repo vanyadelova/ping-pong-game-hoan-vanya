f5ff4b00b5e7d399bec033813af0537dd320e123
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-11-29 21:40:38 +0100

    add baseurl

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:4000' 
+// export const baseUrl = 'http://localhost:4000' 
 
-// export const baseUrl = 'https://dry-island-82304.herokuapp.com/' 
+export const baseUrl = 'https://murmuring-lake-14504.herokuapp.com' 
 export const localStorageJwtKey = 'currentUserJwt'
